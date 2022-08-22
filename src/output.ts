9441d5087018c2994cf39b5aec21d2542d6ce5da
Leonardo Ferreira <66839023+Leoff00@users.noreply.github.com>
2022-08-22 18:31:27 -0300

    Update output.ts

--- src/output.ts
+++ src/output.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 class OutputNewFolder {
   constructor(private readonly Path: string) {}
 
-  fileReaded(): string[] {
+  filesReaded(): string[] {
     const filesReaded = fs.readdirSync(this.Path, "utf-8");
     return filesReaded.filter((files) => files.endsWith(".bin"));
   }
